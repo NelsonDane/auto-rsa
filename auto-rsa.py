9a07f4b6ecf65c4e745592e1f504014f7f893c53
Nelson Dane <danenb@clarkson.edu>
2022-07-24 08:47:34 -0500

    Finish discord mode

--- auto-rsa.py
+++ auto-rsa.py
@@ -25,14 +25,8 @@ if len(sys.argv) > 1:
     wanted_stock = sys.argv[3]
     wanted_time = "day" # Only supports day for now
     wanted_price = "market" # Only supports market for now
-    # Check if DRY mode is enabled
-    print(sys.argv[4])
-    #sleep(100)
-    # if sys.argv[4].lower() == "dry" and not (sys.argv[4].lower() in brokerages):
-    #     print("here")
-    # sleep(100)
+    # Check if DRY mode is enabled   
     if (sys.argv[4].lower()) == "dry" and not (sys.argv[4].lower() in brokerages):
-        print("here")
         DRY = True
         single_broker = "all"
     elif sys.argv[4].lower() in brokerages:
@@ -47,24 +41,30 @@ if len(sys.argv) > 1:
     print(f"Stock: {wanted_stock}")
     print(f"Time: {wanted_time}")
     print(f"Price: {wanted_price}")
-    print(f"DRY: {DRY}")
     print(f"Broker: {single_broker}")
+    print(f"DRY: {DRY}")
+    cli_mode = True
+else:
+    cli_mode = False
 
-sleep(100)
 # Initialize .env file
 load_dotenv()
 
 # Get discord token and prefix from .env file, setting to None if not found
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-if DISCORD_TOKEN:
+if DISCORD_TOKEN and not cli_mode:
     DISCORD = True
 else:
     DISCORD = False
 
 # Raise error if no command line arguments and no discord token
-if len(sys.argv) == 1 and not DISCORD:
+if not cli_mode and not DISCORD:
     print("Error: No command line arguments and no discord token")
     sys.exit(1)
+elif not cli_mode and DISCORD:
+    single_broker = "all"
+    wanted_time = "day"
+    wanted_price = "market"
 
 if DISCORD:
     # Discord bot command prefix
@@ -92,7 +92,7 @@ if single_broker == "all":
     #print()
     robinhood = robinhood_init()
     print()
-    schwab = schwab_init()
+    #schwab = schwab_init()
     print()
     webull_account = webull_init()
     print()
@@ -108,7 +108,7 @@ elif single_broker == "robinhood" or single_broker == "rh":
     robinhood = robinhood_init()
     print()
 elif single_broker == "schwab":
-    schwab = schwab_init()
+    #schwab = schwab_init()
     print()
 elif single_broker == "webull" or single_broker == "wb":
     webull_account = webull_init()
@@ -120,49 +120,76 @@ else:
     print("Error: Invalid broker")
     sys.exit(1)
 
-# # Buy/Sell stock on each account if "all"
-# # Shut up, grammar is important smh
-if wanted_amount > 1:
-    grammar = "shares"
-else:
-    grammar = "share"
-print("==========================================================")
-print(f"Order: {wanted_action} {wanted_amount} {grammar} of {wanted_stock} on {single_broker}")
-print("==========================================================")
-print()
-if single_broker == "all":
-    # Ally
-    ally_transaction(ally_account, wanted_action, wanted_stock, wanted_amount, wanted_price, wanted_time, DRY)
-    # Robinhood
-    robinhood_transaction(robinhood, wanted_action, wanted_stock, wanted_amount, wanted_price, wanted_time, DRY)
-    # Schwab
-    schwab_transaction(schwab, wanted_action, wanted_stock, wanted_amount, wanted_price, wanted_time, DRY)
-    # Webull
-    webull_transaction(webull_account, wanted_action, wanted_stock, wanted_amount, wanted_price, wanted_time, DRY)
-    # Tradier
-    tradier_transaction(tradier, wanted_action, wanted_stock, wanted_amount, wanted_price, wanted_time, DRY)
-elif single_broker == "ally":
-    # Ally
-    ally_transaction(ally_account, wanted_action, wanted_stock, wanted_amount, wanted_price, wanted_time, DRY)
-elif single_broker == "fidelity":
-    # Fidelity
-    #fidelity_transaction(fidelity_user, fidelity_password, wanted_action, wanted_stock, wanted_amount, wanted_price, wanted_time, DRY)
-    print("bruh")
-elif single_broker == "robinhood":
-    # Robinhood
-    robinhood_transaction(robinhood, wanted_action, wanted_stock, wanted_amount, wanted_price, wanted_time, DRY)
-elif single_broker == "schwab":
-    # Schwab
-    schwab_transaction(schwab, wanted_action, wanted_stock, wanted_amount, wanted_price, wanted_time, DRY)
-elif single_broker == "webull":
-    # Webull
-    webull_transaction(webull_account, wanted_action, wanted_stock, wanted_amount, wanted_price, wanted_time, DRY)
-elif single_broker == "tradier":
-    # Tradier
-    tradier_transaction(tradier, wanted_action, wanted_stock, wanted_amount, wanted_price, wanted_time, DRY)
-else:
-    print("Error: Invalid broker")
-    sys.exit(1)
+if DISCORD:
+    print("Waiting for Discord commands...")
+    print()
+
+def place_order(wanted_action, wanted_amount, wanted_stock, single_broker, DRY):
+    try:
+        # Input validation
+        wanted_amount = int(wanted_amount)
+        wanted_stock = wanted_stock.upper()
+        # Shut up, grammar is important smh
+        if wanted_amount > 1:
+            grammar = "shares"
+        else:
+            grammar = "share"
+        print("==========================================================")
+        print(f"Order: {wanted_action} {wanted_amount} {grammar} of {wanted_stock} on {single_broker}")
+        print("==========================================================")
+        print()
+        # Buy/Sell stock on each account if "all"
+        if single_broker == "all":
+            # Ally
+            ally_transaction(ally_account, wanted_action, wanted_stock, wanted_amount, wanted_price, wanted_time, DRY)
+            # Robinhood
+            robinhood_transaction(robinhood, wanted_action, wanted_stock, wanted_amount, wanted_price, wanted_time, DRY)
+            # Schwab
+            #schwab_transaction(schwab, wanted_action, wanted_stock, wanted_amount, wanted_price, wanted_time, DRY)
+            # Webull
+            webull_transaction(webull_account, wanted_action, wanted_stock, wanted_amount, wanted_price, wanted_time, DRY)
+            # Tradier
+            tradier_transaction(tradier, wanted_action, wanted_stock, wanted_amount, wanted_price, wanted_time, DRY)
+        elif single_broker == "ally":
+            # Ally
+            ally_transaction(ally_account, wanted_action, wanted_stock, wanted_amount, wanted_price, wanted_time, DRY)
+        elif single_broker == "fidelity":
+            # Fidelity
+            #fidelity_transaction(fidelity_user, fidelity_password, wanted_action, wanted_stock, wanted_amount, wanted_price, wanted_time, DRY)
+            print("bruh")
+        elif single_broker == "robinhood" or single_broker == "rh":
+            # Robinhood
+            robinhood_transaction(robinhood, wanted_action, wanted_stock, wanted_amount, wanted_price, wanted_time, DRY)
+        elif single_broker == "schwab":
+            # Schwab
+            print("bruh")
+            #schwab_transaction(schwab, wanted_action, wanted_stock, wanted_amount, wanted_price, wanted_time, DRY)
+        elif single_broker == "webull" or single_broker == "wb":
+            # Webull
+            webull_transaction(webull_account, wanted_action, wanted_stock, wanted_amount, wanted_price, wanted_time, DRY)
+        elif single_broker == "tradier":
+            # Tradier
+            tradier_transaction(tradier, wanted_action, wanted_stock, wanted_amount, wanted_price, wanted_time, DRY)
+        else:
+            print("Error: Invalid broker")
+            sys.exit(1)
+    except Exception as e:
+        print(f"Error placing order: {e}")            
+
+if cli_mode and not DISCORD:
+    place_order(wanted_action, wanted_amount, wanted_stock, single_broker, DRY)
+    sys.exit(0)
+elif not cli_mode and DISCORD:
+    @bot.command(name='rsa')
+    async def rsa(ctx, wanted_action, wanted_amount, wanted_stock, wanted_account, DRY):
+        if DRY.lower() == "dry":
+            DRY = True
+        else:
+            DRY = False
+        place_order(wanted_action, wanted_amount, wanted_stock, wanted_account, DRY)
+        print()
+        print("Waiting for Discord commands...")
+        print()
 
 if DISCORD:
     # Run bot
