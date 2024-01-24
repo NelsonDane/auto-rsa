35a17754148da5edae5975f54440c76efec7f902
Nelson Dane <47427072+NelsonDane@users.noreply.github.com>
2024-01-23 23:50:19 -0500

    Merge Develop into Main (#147)
    * add firsttrade
    
    * finish firstrade
    
    * add firstrade to dockerfile
    
    * remove duplicate import
    
    * fix order
    
    * fix incorrect comment
    
    * correct object def
    
    * add vars to readme
    
    * ignore pickle files
    
    * small formatting fix
    
    * add traceback
    
    * change ft api version
    
    * fix firstrade buy only
    
    * change ft_api version
    
    * fix firstrade api to use acount dict
    
    * update ft_api version
    
    * change ft_api version
    
    * remove redundant variable
    
    * code cleanup
    
    * Deepsource style fix
    
    * add new print to ft
    
    * update readme
    
    * style: format code with Black and isort
    
    This commit fixes the style issues introduced in c57833a according to the output
    from Black and isort.
    
    Details: None
    
    * update mask string
    
    * style: format code with Black and isort
    
    This commit fixes the style issues introduced in 7b0fb19 according to the output
    from Black and isort.
    
    Details: None
    
    * fidelity fix
    
    * fix import styling
    
    * revert import order
    
    * last import order fix
    
    * remove unused prices
    
    * bad indent
    
    * style: format code with Black and isort
    
    This commit fixes the style issues introduced in 57b3cde according to the output
    from Black and isort.
    
    Details: None
    
    ---------
    
    Co-authored-by: Donald Ryan Gullett <45299186+MaxxRK@users.noreply.github.com>
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>
    Co-authored-by: maxxrk <drgullett@azsmail.com>

--- fidelityAPI.py
+++ fidelityAPI.py
@@ -112,8 +112,10 @@ def fidelity_init(FIDELITY_EXTERNAL=None, DOCKER=False):
             # Retry the login if we get an error page
             try:
                 go_back_selector = "#dom-sys-err-go-to-login-button > span > s-slot > s-assigned-wrapper"
-                WebDriverWait(driver,10).until(
-                    expected_conditions.element_to_be_clickable((By.CSS_SELECTOR, go_back_selector)),
+                WebDriverWait(driver, 10).until(
+                    expected_conditions.element_to_be_clickable(
+                        (By.CSS_SELECTOR, go_back_selector)
+                    ),
                 ).click()
                 username_field = driver.find_element(
                     by=By.CSS_SELECTOR, value=username_selector
@@ -123,7 +125,9 @@ def fidelity_init(FIDELITY_EXTERNAL=None, DOCKER=False):
                     by=By.CSS_SELECTOR, value=password_selector
                 )
                 type_slowly(password_field, account[1])
-                driver.find_element(by=By.CSS_SELECTOR, value=login_btn_selector).click()
+                driver.find_element(
+                    by=By.CSS_SELECTOR, value=login_btn_selector
+                ).click()
             except TimeoutException:
                 pass
             # Wait for page to load to summary page
@@ -356,27 +360,14 @@ def fidelity_transaction(fidelity_o: Brokerage, orderObj: stockOrder, loop=None)
                         return
                     except Exception:
                         pass
-                    # Get ask/bid price
-                    ask_price = (
-                        driver.find_element(
-                            by=By.CSS_SELECTOR,
-                            value="#quote-panel > div > div.eq-ticket__quote--blocks-container > div:nth-child(2) > div > span > span",
-                        )
-                    ).text
-                    # Last price gets us a more accurate number
+                    # Get last price
                     last_price = driver.find_element(
                         by=By.CSS_SELECTOR,
-                        value="#eq-ticket__last-price > span.last-price"
+                        value="#eq-ticket__last-price > span.last-price",
                     ).text
                     last_price = last_price.replace("$", "")
-                    bid_price = (
-                        driver.find_element(
-                            by=By.CSS_SELECTOR,
-                            value="#quote-panel > div > div.eq-ticket__quote--blocks-container > div:nth-child(1) > div > span > span",
-                        )
-                    ).text
                     # If price is under $1, then we have to use a limit order
-                    LIMIT = bool(float(ask_price) < 1 or float(bid_price) < 1)
+                    LIMIT = bool(float(last_price) < 1)
                     # Figure out whether page is in old or new style
                     try:
                         action_dropdown = driver.find_element(
@@ -462,10 +453,15 @@ def fidelity_transaction(fidelity_o: Brokerage, orderObj: stockOrder, loop=None)
                             limit_button.click()
                         # Set price
                         difference_price = 0.01 if float(last_price) > 0.1 else 0.0001
+                        difference_price = 0.01 if float(last_price) > 0.1 else 0.0001
                         if orderObj.get_action() == "buy":
-                            wanted_price = round(float(last_price) + difference_price, 3)
+                            wanted_price = round(
+                                float(last_price) + difference_price, 3
+                            )
                         else:
-                            wanted_price = round(float(last_price) - difference_price, 3)
+                            wanted_price = round(
+                                float(last_price) - difference_price, 3
+                            )
                         if new_style:
                             price_box = driver.find_element(
                                 by=By.CSS_SELECTOR, value="#eqt-mts-limit-price"
