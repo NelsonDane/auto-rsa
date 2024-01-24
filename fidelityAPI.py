6762ec43ba285454bfc733f2a39caf54a453abd6
maxxrk <drgullett@azsmail.com>
2024-01-23 19:30:57 -0600

    fidelity fix

--- fidelityAPI.py
+++ fidelityAPI.py
@@ -9,24 +9,15 @@ import traceback
 from time import sleep
 
 from dotenv import load_dotenv
+from helperAPI import (Brokerage, check_if_page_loaded, getDriver,
+                       killSeleniumDriver, maskString, printAndDiscord,
+                       printHoldings, stockOrder, type_slowly)
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
-
-from helperAPI import (
-    Brokerage,
-    check_if_page_loaded,
-    getDriver,
-    killSeleniumDriver,
-    maskString,
-    printAndDiscord,
-    printHoldings,
-    stockOrder,
-    type_slowly,
-)
 
 
 def fidelity_error(driver: webdriver, error: str):
@@ -112,10 +103,8 @@ def fidelity_init(FIDELITY_EXTERNAL=None, DOCKER=False):
             # Retry the login if we get an error page
             try:
                 go_back_selector = "#dom-sys-err-go-to-login-button > span > s-slot > s-assigned-wrapper"
-                WebDriverWait(driver, 10).until(
-                    expected_conditions.element_to_be_clickable(
-                        (By.CSS_SELECTOR, go_back_selector)
-                    ),
+                WebDriverWait(driver,10).until(
+                    expected_conditions.element_to_be_clickable((By.CSS_SELECTOR, go_back_selector)),
                 ).click()
                 username_field = driver.find_element(
                     by=By.CSS_SELECTOR, value=username_selector
@@ -125,9 +114,7 @@ def fidelity_init(FIDELITY_EXTERNAL=None, DOCKER=False):
                     by=By.CSS_SELECTOR, value=password_selector
                 )
                 type_slowly(password_field, account[1])
-                driver.find_element(
-                    by=By.CSS_SELECTOR, value=login_btn_selector
-                ).click()
+                driver.find_element(by=By.CSS_SELECTOR, value=login_btn_selector).click()
             except TimeoutException:
                 pass
             # Wait for page to load to summary page
