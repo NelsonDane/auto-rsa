a286671d7e16108842eeb0a1c8eb3d9ea499fe04
maxxrk <drgullett@azsmail.com>
2024-01-23 19:29:34 -0600

    fidelity fixes

--- fidelityAPI.py
+++ fidelityAPI.py
@@ -9,24 +9,15 @@ import traceback
 from time import sleep
 
 from dotenv import load_dotenv
+from helperAPI import (Brokerage, check_if_page_loaded, getDriver,
+                       killSeleniumDriver, maskString, printAndDiscord,
+                       printHoldings, stockOrder, type_slowly)
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
-
-from helperAPI import (
-    Brokerage,
-    check_if_page_loaded,
-    getDriver,
-    killSeleniumDriver,
-    maskString,
-    printAndDiscord,
-    printHoldings,
-    stockOrder,
-    type_slowly,
-)
 
 
 def fidelity_error(driver: webdriver, error: str):
@@ -109,6 +100,23 @@ def fidelity_init(FIDELITY_EXTERNAL=None, DOCKER=False):
             driver.find_element(by=By.CSS_SELECTOR, value=login_btn_selector).click()
             WebDriverWait(driver, 10).until(check_if_page_loaded)
             sleep(3)
+            # Retry the login if we get an error page
+            try:
+                go_back_selector = "#dom-sys-err-go-to-login-button > span > s-slot > s-assigned-wrapper"
+                WebDriverWait(driver,10).until(
+                    expected_conditions.element_to_be_clickable((By.CSS_SELECTOR, go_back_selector)),
+                ).click()
+                username_field = driver.find_element(
+                    by=By.CSS_SELECTOR, value=username_selector
+                )
+                type_slowly(username_field, account[0])
+                password_field = driver.find_element(
+                    by=By.CSS_SELECTOR, value=password_selector
+                )
+                type_slowly(password_field, account[1])
+                driver.find_element(by=By.CSS_SELECTOR, value=login_btn_selector).click()
+            except TimeoutException:
+                pass
             # Wait for page to load to summary page
             if "summary" not in driver.current_url:
                 if "errorpage" in driver.current_url.lower():
@@ -346,6 +354,12 @@ def fidelity_transaction(fidelity_o: Brokerage, orderObj: stockOrder, loop=None)
                             value="#quote-panel > div > div.eq-ticket__quote--blocks-container > div:nth-child(2) > div > span > span",
                         )
                     ).text
+                    # Last price gets us a more accurate number
+                    last_price = driver.find_element(
+                        by=By.CSS_SELECTOR,
+                        value="#eq-ticket__last-price > span.last-price"
+                    ).text
+                    last_price = last_price.replace("$", "")
                     bid_price = (
                         driver.find_element(
                             by=By.CSS_SELECTOR,
@@ -438,11 +452,11 @@ def fidelity_transaction(fidelity_o: Brokerage, orderObj: stockOrder, loop=None)
                             )
                             limit_button.click()
                         # Set price
-                        difference_price = 0.01 if float(ask_price) > 0.1 else 0.001
+                        difference_price = 0.01 if float(last_price) > 0.1 else 0.0001
                         if orderObj.get_action() == "buy":
-                            wanted_price = round(float(ask_price) + difference_price, 3)
+                            wanted_price = round(float(last_price) + difference_price, 3)
                         else:
-                            wanted_price = round(float(bid_price) - difference_price, 3)
+                            wanted_price = round(float(last_price) - difference_price, 3)
                         if new_style:
                             price_box = driver.find_element(
                                 by=By.CSS_SELECTOR, value="#eqt-mts-limit-price"
