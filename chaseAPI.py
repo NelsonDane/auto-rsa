1a4daf6c207ac2f10d80609aa41302c2f382cb72
Maxxrk <drgullett@azsmail.com>
2024-02-16 08:44:48 -0600

    fix chase loop

--- chaseAPI.py
+++ chaseAPI.py
@@ -92,7 +92,7 @@ def chase_holdings(chase_o: Brokerage, loop=None):
                 if h == 0:
                     all_accounts = ch_account.AllAccount(obj)
                     if all_accounts is None:
-                            raise Exception(f"Error getting account details")    
+                        raise Exception("Error getting account details")    
                 account_id = get_account_id(all_accounts.account_connectors, account)
                 data = symbols.SymbolHoldings(account_id, obj)
                 success = data.get_holdings()
@@ -143,7 +143,7 @@ def chase_transaction(chase_o: Brokerage, orderObj: stockOrder, loop=None):
                     if i == 0:
                         all_accounts = ch_account.AllAccount(obj)
                         if all_accounts is None:
-                            raise Exception(f"Error getting account details")         
+                            raise Exception("Error getting account details")         
                     account_id = get_account_id(all_accounts.account_connectors, account)
                     # If DRY is True, don't actually make the transaction
                     if orderObj.get_dry():
