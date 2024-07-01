cb6924a64b11fc9067a2f190c005187029ab8228
Mukundh <tpamukundh@gmail.com>
2024-07-01 04:55:26 -0400

    wells fargo transaction

--- wellsfargoAPI.py
+++ wellsfargoAPI.py
@@ -88,5 +88,96 @@ def wellsfargo_init(WELLSFARGO_EXTERNAL=None,DOCKER=Fasle):
             return None
     return WELLSFARGO_obj
 
+def wellsfargo_transaction(WELLSFARGO_o:Brokerage,orderObj:stockOrder,loop=None,DOCKER=False):
+    print()
+    print("==============================")
+    print("WELLS FARGO")
+    print("==============================")
+    print()
+
+    driver=DRIVER
+
+    driver.get(WELLSFARGO_o.get_url)
     
+    #Navigate to Trade
+    try:
+        brokerage = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='BROKERAGE_LINK7P']")))
+        brokerage.click()
+
+        trade = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='trademenu']/span[1]")))
+        trade.click()
+
+        trade_stock = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='linktradestocks']")))
+        trade_stock.click()
+
+        #Find accounts
+        open_dropdown = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='dropdown2']")))
+        open_dropdown.click()
+
+        accounts = driver.execute_script("return document.getElementById('dropdownlist2').getElementsByTagName('li').length;")
+        accounts = int(accounts)
+    except TimeoutException:
+        print("could not get to trade")
+        killSeleniumDriver(WELLSFARGO_o)
+
+    for account in range(accounts):
+        try:
+            #choose account
+            open_dropdown.click()
+            driver.execute_script("document.getElementById('dropdownlist2').getElementsByTagName('li')["+ str(account) +"].click()")
+        except:
+            print("could not change account")
+            killSeleniumDriver(WELLSFARGO_o)
+
+        for s in orderObj.get_stocks():
+
+            driver.execute_script('document.getElementById("BuySellBtn").click()')
+            #Buy or Sell
+            action = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.LINK_TEXT, orderObj.get_action())))
+            action.click()
+            
+            # ticker
+            tickerBox = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "Symbol")))
+            tickerBox.send_keys(s)
+            tickerBox.send_keys(Keys.ENTER)
+
+            # quantity
+            quantBox = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "OrderQuantity")))
+            quantBox.send_keys(orderObj.get_amount())
+            quantBox.send_keys(Keys.ENTER)
+
+            # order type
+            orderBox = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "OrderTypeBtnText")))
+            orderBox.click()
+
+            order = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "Limit")))
+            order.click()
+
+            # limit price
+            tickerBox = driver.find_element(By.ID, "Price")
+            tickerBox.send_keys(ticker[1])
+            tickerBox.send_keys(Keys.ENTER)
+
+            # timing
+            timing = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "TIFBtn")))
+            timing.click()
+            day = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "Day")))
+            day.click()
+
+            # preview
+            review = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "actionbtnContinue")))
+            review.click()
+
+            # submit
+            submit = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn-wfa-submit")))
+            submit.click()
+
+            buy_next = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn-wfa-primary")))
+            buy_next.click()
+
+
+
+
+
+
 
