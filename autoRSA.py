58095874f2b6f68456384ec3e5148dadad3abcaf
Mukundh <tpamukundh@gmail.com>
2024-08-10 19:26:14 -0400

    cleaned up

--- autoRSA.py
+++ autoRSA.py
@@ -178,9 +178,7 @@ def argParser(args: list) -> stockOrder:
         elif args[1] == "day1":
             orderObj.set_brokers(DAY1_BROKERS)
         elif args[1] == "most":
-            orderObj.set_brokers(
-                list(filter(lambda x: x != 'vanguard', SUPPORTED_BROKERS))
-            )
+            orderObj.set_brokers(list(filter(lambda x: x != 'vanguard', SUPPORTED_BROKERS)))
         elif args[1] == "fast":
             orderObj.set_brokers(DAY1_BROKERS + ["robinhood"])
         else:
