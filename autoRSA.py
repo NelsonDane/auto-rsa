ce2ffbbdab0e98391baad30ee8eaa4d8c17e4c33
Nelson Dane <47427072+NelsonDane@users.noreply.github.com>
2024-08-21 12:50:15 -0400

    check python version at start

--- autoRSA.py
+++ autoRSA.py
@@ -227,6 +227,12 @@ def argParser(args: list) -> stockOrder:
 
 
 if __name__ == "__main__":
+    # Check Python version (minimum 3.10)
+    print("Python version:", sys.version)
+    if sys.version_info < (3, 10):
+        print("Python 3.10 or newer is required")
+        sys.exit(1)
+    print()
     # Determine if ran from command line
     if len(sys.argv) == 1:  # If no arguments, do nothing
         print("No arguments given, see README for usage")
