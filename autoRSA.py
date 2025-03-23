3d1e6cb4cbf2f67d2a41a605bfc264d18d2217b1
Nelson Dane <47427072+NelsonDane@users.noreply.github.com>
2025-03-23 15:39:00 -0400

    single if statement

--- autoRSA.py
+++ autoRSA.py
@@ -367,7 +367,7 @@ if __name__ == "__main__":
         # Bypass process_commands to allow bot messages
         @bot.event
         async def on_message(message):
-            if message.channel.id == DISCORD_CHANNEL:
+            if message.channel.id == DISCORD_CHANNEL and message.author != bot.user:
                 ctx = await bot.get_context(message)
                 # the type of the invocation context's bot attribute will be correct
                 await bot.invoke(ctx)  # type: ignore
